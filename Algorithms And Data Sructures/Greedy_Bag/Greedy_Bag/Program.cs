72d71df49d2a857f874f68d4af4a14552936fc3f
ElectronicBg <76813158+ElectronicBg@users.noreply.github.com>
2023-09-22 01:21:48 +0300

    Example Documentation/ CRUD Operations/  Greedy Bag

---
+++ Algorithms And Data Sructures/Greedy_Bag/Greedy_Bag/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+
+namespace Greedy_Bag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] weights = { 2, 3, 4, 5 };
+            int[] values = { 3, 4, 5, 6 };
+            int capacity = 15;
+
+            // Solve the knapsack problem using the greedy algorithm
+            int[] selectedItems = GreedyKnapsack(weights, values, capacity);
+
+            // Print the selected items
+            Console.WriteLine("Selected items:");
+            for (int i = 0; i < selectedItems.Length; i++)
+            {
+                Console.WriteLine($"Item {i + 1}: {(selectedItems[i] == 1 ? "Selected" : "Not selected")}");
+            }
+        }
+
+        // Greedy algorithm to solve the 0/1 Knapsack problem
+        static int[] GreedyKnapsack(int[] weights, int[] values, int capacity)
+        {
+            int n = weights.Length;
+            int[] selectedItems = new int[n];
+            double[] valuePerWeight = new double[n];
+
+            // Calculate the value-to-weight ratio for each item
+            for (int i = 0; i < n; i++)
+            {
+                valuePerWeight[i] = (double)values[i] / weights[i];
+            }
+
+            // Sort items based on the value-to-weight ratio in decreasing order
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (valuePerWeight[i] < valuePerWeight[j])
+                    {
+                        // Swap items to maintain descending order
+                        swap(ref valuePerWeight[i], ref valuePerWeight[j]);
+                        swap(ref weights[i], ref weights[j]);
+                        swap(ref values[i], ref values[j]);
+                    }
+                }
+            }
+
+            int currentWeight = 0;
+
+            // Fill the knapsack with selected items
+            for (int i = 0; i < n; i++)
+            {
+                if (currentWeight + weights[i] <= capacity)
+                {
+                    selectedItems[i] = 1;
+                    currentWeight += weights[i];
+                }
+                else
+                {
+                    selectedItems[i] = 0;
+                }
+            }
+
+            return selectedItems;
+        }
+
+        // Helper method to swap two values of any type
+        static void swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
---
+++ Internet Programming/CRUD_Operations/CRUD_Operations/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUD_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+
+            View();
+
+            int input = int.Parse(Console.ReadLine());
+            while (input != 6)
+            {
+                switch (input)
+                {
+                    case 1:
+                        Read(myDictionary);
+                        View();
+                        break;
+                    case 2:
+                        GetItemById(myDictionary);
+                        View();
+                        break;
+                    case 3:
+                        Create(myDictionary);
+                        View();
+                        break;
+                    case 4:
+                        UpdateItemById(myDictionary);
+                        View();
+                        break;
+                    case 5:
+                        DeleteItemById(myDictionary);
+                        View();
+                        break;
+                }
+                input = int.Parse(Console.ReadLine());
+            }
+
+        }
+        public static void Create(Dictionary<int, string> dictionary)
+        {
+            int id = dictionary.Count + 1;
+            Console.WriteLine("Enter item name: ");
+
+            string value = Console.ReadLine();
+            Console.WriteLine($"{value} added with id = {id}");
+
+            dictionary.Add(id, value);
+        }
+        public static void Read(Dictionary<int, string> dictionary)
+        {
+            foreach (var kvp in dictionary)
+            {
+                Console.WriteLine($"ID: {kvp.Key}, Value: {kvp.Value}");
+            }
+        }
+        public static void GetItemById(Dictionary<int, string> dictionary)
+        {
+            Console.WriteLine("Enter item  ID: ");
+            int id = int.Parse(Console.ReadLine());
+
+            if (dictionary.ContainsKey(id))
+            {
+                Console.WriteLine($"{dictionary[id]}");
+            }
+            else
+            {
+                Console.WriteLine($"Item with ID {id} doesn't exist!");
+            }
+        }
+        public static void UpdateItemById(Dictionary<int, string> dictionary)
+        {
+            Console.WriteLine("Enter item  ID: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter item new value: ");
+            string newItemValue = Console.ReadLine();
+
+            if (dictionary.ContainsKey(id))
+            {
+                dictionary[id] = newItemValue;
+                Console.WriteLine($"Item with ID {id} has been updated.");
+            }
+            else
+            {
+                Console.WriteLine($"Item with ID {id} doesn't exist!");
+            }
+        }
+        public static void DeleteItemById(Dictionary<int, string> dictionary)
+        {
+            Console.WriteLine("Enter item  ID to delete: ");
+            int id = int.Parse(Console.ReadLine());
+
+            if (dictionary.ContainsKey(id))
+            {
+                dictionary.Remove(id);
+                Console.WriteLine($"Item with ID {id} has been deleted.");
+            }
+            else
+            {
+                Console.WriteLine($"Item with ID {id} doesn't exist!");
+            }
+        }
+        public static void View()
+        {
+            Console.WriteLine("Choose an action:");
+            Console.WriteLine("1. GET all items");
+            Console.WriteLine("2. GET item by ID");
+            Console.WriteLine("3. POST a new item");
+            Console.WriteLine("4. PUT update item by ID");
+            Console.WriteLine("5. DELETE item by ID");
+            Console.WriteLine("6. Exit");
+        }
+    }
+}
+
