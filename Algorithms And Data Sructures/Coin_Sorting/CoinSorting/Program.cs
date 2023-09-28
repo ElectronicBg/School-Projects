00af663e64bddae92c45ae6cff5a5141b0b9b49d
EnergoPro <electronigaming@gmail.com>
2023-09-28 09:22:22 +0300

    Coin_Sorting

---
+++ Algorithms And Data Sructures/Coin_Sorting/CoinSorting/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Coin_Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define the available coin denominations.
+            double[] coins = { 1, 2, 5, 10, 20, 50 };
+
+            // Get the desired value from the user.
+            Console.Write("Enter the desired value: ");
+            double wantedValue = double.Parse(Console.ReadLine());
+
+            // Initialize variables.
+            int coinsLength = coins.Length - 1;
+            List<int> wantedCoins = new List<int>();
+
+            // Iterate through the coin denominations.
+            while (coinsLength >= 0)
+            {
+                if (wantedValue / coins[coinsLength] < 1)
+                {
+                    // If the current coin is too large, move to the next smaller coin.
+                    coinsLength -= 1;
+                }
+                else
+                {
+                    // Subtract the coin value from the desired value.
+                    wantedValue -= coins[coinsLength];
+
+                    // Add the coin to the list of wanted coins.
+                    wantedCoins.Add((int)coins[coinsLength]);
+                }
+            }
+
+            // Print the list of wanted coins.
+            Console.WriteLine("Selected coins:");
+            Console.WriteLine(string.Join(" ", wantedCoins));
+        }
+    }
+}
