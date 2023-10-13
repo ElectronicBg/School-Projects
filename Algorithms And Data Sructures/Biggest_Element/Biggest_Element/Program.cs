8ac80ecdcc5e507fba8030d0cf745710aeaa9b51
EnergoPro <electronigaming@gmail.com>
2023-10-13 14:05:32 +0300

    Biggest_Element/Factorial_With_Recursion

---
+++ Algorithms And Data Sructures/Biggest_Element/Biggest_Element/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a list of numbers separated by spaces:");
+        string input = Console.ReadLine();
+
+        List<int> myList = ParseInput(input);
+
+        if (myList.Count > 0)
+        {
+            int maxElement = FindMaxRecursive(myList);
+            Console.WriteLine("The largest element in the list is: " + maxElement);
+        }
+        else
+        {
+            Console.WriteLine("The list is empty.");
+        }
+    }
+
+    static List<int> ParseInput(string input)
+    {
+        List<int> list = new List<int>();
+        string[] tokens = input.Split(' ');
+
+        foreach (string token in tokens)
+        {
+            if (int.TryParse(token, out int number))
+            {
+                list.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input: " + token + " is not a valid number and will be ignored.");
+            }
+        }
+
+        return list;
+    }
+
+    static int FindMaxRecursive(List<int> list)
+    {
+        if (list.Count == 0)
+            return int.MinValue;
+
+        if (list.Count == 1)
+            return list[0];
+
+        int mid = list.Count / 2;
+        List<int> leftHalf = list.GetRange(0, mid);
+        List<int> rightHalf = list.GetRange(mid, list.Count - mid);
+
+        int maxLeft = FindMaxRecursive(leftHalf);
+        int maxRight = FindMaxRecursive(rightHalf);
+
+        return Math.Max(maxLeft, maxRight);
+    }
+}
---
+++ Algorithms And Data Sructures/Factorial_With_Recursion/Factorial_With_Recursion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number to calculate its factorial: ");
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            if (number < 0)
+            {
+                Console.WriteLine("Factorial is not defined for negative numbers.");
+            }
+            else
+            {
+                long factorial = CalculateFactorial(number);
+                Console.WriteLine($"The factorial of {number} is {factorial}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+    }
+
+    static long CalculateFactorial(int n)
+    {
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+        return n * CalculateFactorial(n - 1);
+    }
+}
