0e32c0364eb849315ed35d6ccff0d145fcc295a6
ElectronicBg <76813158+ElectronicBg@users.noreply.github.com>
2023-10-18 21:19:46 +0300

    Finished MedCare/ Added Towers_of_Hanoi and Egyptian_Fractions

---
+++ Algorithms And Data Sructures/Egyptian_Fractions/Egyptian_Fractions/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Egyptian_Fractions
+{
+    static void Main()
+    {
+        Console.Write("Enter the numerator a: ");
+        int numerator = int.Parse(Console.ReadLine());
+        Console.Write("Enter the denominator b: ");
+        int denominator = int.Parse(Console.ReadLine());
+
+        if (numerator >= denominator || numerator <= 0 || denominator <= 0)
+        {
+            Console.WriteLine("Invalid input. The numerator must be smaller than the denominator, and both numbers must be positive.");
+            return;
+        }
+
+        List<int> egyptianFractions = FindEgyptianFractions(numerator, denominator);
+
+        Console.WriteLine($"The representation of {numerator}/{denominator} as a sum of Egyptian fractions is:");
+        for (int i = 0; i < egyptianFractions.Count; i++)
+        {
+            if (i == egyptianFractions.Count - 1)
+            {
+                Console.Write("1/" + egyptianFractions[i]);
+            }
+            else
+            {
+                Console.Write("1/" + egyptianFractions[i] + " + ");
+            }
+        }
+    }
+
+    static List<int> FindEgyptianFractions(int numerator, int denominator)
+    {
+        List<int> egyptianFractions = new List<int>();
+
+        while (numerator != 0)
+        {
+            int nextDenominator = (denominator + numerator - 1) / numerator;
+            egyptianFractions.Add(nextDenominator);
+
+            numerator = numerator * nextDenominator - denominator;
+            denominator = denominator * nextDenominator;
+
+            int gcd = GCD(numerator, denominator);
+            numerator /= gcd;
+            denominator /= gcd;
+        }
+
+        return egyptianFractions;
+    }
+
+    static int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
---
+++ Algorithms And Data Sructures/Towers_of_Hanoi/Towers_of_Hanoi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Towers_Of_Hanoi
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number of disks: ");
+        int numDisks = int.Parse(Console.ReadLine());
+
+        TowerOfHanoi(numDisks, 'A', 'C', 'B');
+    }
+
+    static void TowerOfHanoi(int n, char source, char destination, char auxiliary)
+    {
+        if (n == 1)
+        {
+            Console.WriteLine($"Move disk 1 from {source} to {destination}");
+            return;
+        }
+
+        TowerOfHanoi(n - 1, source, auxiliary, destination);
+        Console.WriteLine($"Move disk {n} from {source} to {destination}");
+        TowerOfHanoi(n - 1, auxiliary, destination, source);
+    }
+}
