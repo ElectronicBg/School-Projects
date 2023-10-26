d678b2d4dcd0004059707569b4a7aa77918bb795
EnergoPro <electronigaming@gmail.com>
2023-10-26 13:27:10 +0300

    Sum_Of_Sequence/Cube_Of_Number

---
+++ Algorithms And Data Sructures/Cube_Of_Number/Cube_Of_Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cube_Of_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Number is {i} and cube of the {i} is: {Math.Pow(i,3)} ");
+            }
+        }
+    }
+}
---
+++ Algorithms And Data Sructures/Sum_Of_Sequence/Sum_Of_Sequence/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sum_Of_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            int sum = 0;
+            int currentTerm = 1; 
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == n)
+                {
+                    Console.Write(currentTerm);
+                }
+                else
+                {
+                    Console.Write(currentTerm + "+");
+                }
+
+                sum += currentTerm; 
+                currentTerm = currentTerm * 10 + 1; 
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Sum: " + sum);
+
+        }
+    }
+}
