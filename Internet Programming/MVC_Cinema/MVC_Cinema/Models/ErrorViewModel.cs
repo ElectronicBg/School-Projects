f4679909e4acd7a70df3c962c6c495a6a7908e88
EnergoPro <electronigaming@gmail.com>
2023-11-30 10:23:20 +0200

    Stuff

--- Internet Programming/MVC_Cinema/MVC_Cinema/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC_Cinema.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
